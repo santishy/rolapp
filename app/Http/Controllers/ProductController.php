bedf2c6af1e692a6fe0c3309645ce5f145756604
santiago martin <santiagomartinochoaestrada@gmail.com>
2020-09-21 15:59:45 -0500

    creando el controlador product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -10,7 +11,16 @@ class ProductController extends Controller
         return view('dashboard.products.create');
     }
 
-    public function store(){
-        return request()->all();
+    public function store(Request $request){
+        $request->file->store('songs');
+    }
+
+    public function validateProduct($data){
+        Validator::make($request->all(),
+            [
+                'title' => 'unique:products,title|required'
+                'description'
+            ] 
+        );
     }
 }
