e5954727cb7af1977268835a9b891f03f046702b
santiago martin <santiagomartinochoaestrada@gmail.com>
2020-09-23 15:38:37 -0500

    creando el metodo store en ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,25 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         return view('dashboard.products.create');
     }
 
-    public function store(Request $request){
-        $request->file->store('songs');
+    public function store(Request $request)
+    {
+        $this->validateProduct($request);
+        $request->file = $request->file->store('songs');
+        return response()->json([
+            'product' =>  Product::create($request->all())
+        ]);
     }
 
-    public function validateProduct($data){
-        Validator::make($request->all(),
-            [
-                'title' => 'unique:products,title|required'
-                'description'
-            ] 
-        );
+    public function validateProduct($request)
+    {
+        return $request->validate([
+            'title' => 'unique:products,title|required',
+            'description' => 'required',
+            'file' => 'required|file|mimes:mpga,mp2,mp2a,mp3,m2a,m3a',
+            'price' => 'required|Numeric'
+        ]);
     }
 }
