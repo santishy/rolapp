0ea43c6087f1abbf3ff5daff9da04cf2e40b59af
santiago martin <santiagomartinochoaestrada@gmail.com>
2020-10-11 14:57:07 -0500

    casi finalizando con paypal

--- app/Paypal.php
+++ app/Paypal.php
@@ -4,6 +4,8 @@ namespace App;
 
 use App\Traits\ConsumeExternalService;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
 
 class Paypal extends Model
 {
@@ -19,6 +21,12 @@ class Paypal extends Model
         $this->baseUri = config('services.paypal.base_uri');
         $this->clientId = config('services.paypal.client_id');
         $this->clientSecret = config('services.paypal.client_secret');
+    }
+    public function handlePayment(Request $request){
+        $order = $this->createOrder($request->value,'mxn');
+        $links = collect($order->links);
+        $approve = $links->where('rel','approve')->first();
+        redirect($approve->href);
     }
     public function resolveAuthorization(&$queryParams, &$formParams, &$headers)
     {
@@ -58,5 +66,15 @@ class Paypal extends Model
             $isJsonRequest = true
         );
         return $response;
+    }
+
+    public function capturePayment($order_id){
+        return $this->makeRequest(
+            'POST',
+            "/v2/checkout/orders/{$order_id}/capture",
+            [],
+            [],
+            ['Content-Type' => 'application/json']
+        );
     }
 }
