497eece6f074824242153991861b70058712bac3
nicojl <njl27@hotmail.com>
2021-01-25 18:43:43 -0600

    start home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            Aquí se comienza a construir la pagina de inicio ...
-        </div>
+  <div class="row">
+    <div class="col col-lg-4 col-md-6 col-12 text-center">
+      <img class="img-fluid align-middle" src="./images/logo.png" alt="logo" />
     </div>
+    <div class="col col-lg-8 col-md-6 col-12 text-center">
+      <video autoplay muted loop id="myVideo">
+        <source src="video/video.mp4" type="video/mp4">
+      </video>
+    </div>
+  </div>
 </div>
 @endsection
+
