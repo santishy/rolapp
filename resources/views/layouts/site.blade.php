b5ded4edc41c70ce0df77fd90edf15b958a17256
nicojl <njl27@hotmail.com>
2021-04-10 00:37:52 -0500

    ocultando videos

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -60,7 +60,7 @@
             <li class="nav-item">
               <a class="nav-link" href="{{ url('albums') }}">CANCIONES</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item d-none">
               <a class="nav-link" href="{{ url('videos') }}">VIDEOS</a>
             </li>
             <li class="nav-item d-none">
