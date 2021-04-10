8d0793db041884a3e694b09c053f95ed322e0a70
nicojl <njl27@hotmail.com>
2021-04-10 00:35:24 -0500

    quitando video

--- resources/views/videos/aux.blade.php
+++ resources/views/videos/aux.blade.php
@@ -12,7 +12,7 @@
 		<div class="col col-12 col-lg-4 col-sm-6">
 			
 			<h5 class="text-light text-center h5">El Que Más Te Ha Amado</h5>
-			<iframe src="https://www.youtube.com/embed/OSpehzEf5JM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			
 		</div>
 		
 	</div>
