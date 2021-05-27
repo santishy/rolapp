522428fef49133a2c75ed22169f0665cb4498b2a
nicojl <njl27@hotmail.com>
2021-05-27 18:19:52 -0500

    se agrego mix de muestra a la tienda y cambio de color de botón de paypal

--- resources/views/songs/sales.blade.php
+++ resources/views/songs/sales.blade.php
@@ -8,6 +8,20 @@
         <div class="col col-12 text-center">
             <h1 class="mb-4 cursive-custom text-light border-bottom">TIENDA</h1>
         </div>
+				<div class="col col-12 mb-5">
+					<div class="card">
+						<h5 class="card-title text-center mt-2 font-weight-bold">MIX DE MUESTRA</h5>
+						<div class="card-body">
+							<audio class="custom-audio" data-id="0" controls style="width:100%;">
+								<source src="canciones/mix.mp3" type="audio/mpeg">
+							 Tu navegador no soporta elementos de audio.
+							</audio>
+						</div>
+						<div class="card-footer">
+							<p>Las canciones se reproducen en el orden que se muestran a continuación.</p>
+						</div>
+					</div>
+				</div>
         @foreach((object)$albums->resolve() as $album)
 					@foreach ($album['products'] as $product )
 						<div class="col col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
@@ -16,10 +30,7 @@
 											<h5 class="card-title text-center">{{$product->title}}</h5>
 									</div>
 									<div class="card-body">
-											<audio class="custom-audio d-none" controls style="width:100%;" controlsList="nodownload">
-													<source src="{{$product->file}}" type="audio/mpeg">
-													Your browser does not support the audio element.
-											</audio>
+											
 											<p class="text-center mt-2 font-weight-bold">COSTO: @money($product->price)</p>
 									</div>
 								</div>
@@ -29,7 +40,7 @@
 											<input type="hidden" value="{{$product->price}}" name="value">
 											<input type="hidden" value="{{$product->id}}" name="product_id">
 											@csrf
-											<button class="btn btn-success btn-block font-weight-bold float-right" type="submit"><i class="fa fa-paypal" aria-hidden="true"></i> COMPRAR</button>
+											<button class="btn btn-paypal btn-block font-weight-bold float-right" type="submit"><i class="fa fa-paypal" aria-hidden="true"></i> COMPRAR</button>
 									</form>
 								</div>
 						</div>
