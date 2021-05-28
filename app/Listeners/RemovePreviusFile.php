0f3715f1801006585c1692ef515b8b892767469b
nicojl <njl27@hotmail.com>
2021-05-28 17:43:31 -0500

    cambiando remove previus file

--- app/Listeners/RemovePreviusFile.php
+++ app/Listeners/RemovePreviusFile.php
@@ -28,6 +28,6 @@ class RemovePreviusFile
      */
     public function handle(FileUpdated $event)
     {
-        Storage::delete(Storage::delete($event->file_uri));
+        Storage::delete($event->file_uri);
     }
 }
