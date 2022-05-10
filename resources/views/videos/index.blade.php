0a82d81ee735d81e9a34904c0bc5a0e7786007d9
nicojl <njl27@hotmail.com>
2022-05-09 20:15:12 -0500

    videos como componer

--- resources/views/videos/index.blade.php
+++ resources/views/videos/index.blade.php
@@ -17,6 +17,14 @@
 			<h5 class="text-light text-center h5">Entrevista Xochitl Radio</h5>
 			<iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FXochitl.Radio%2Fvideos%2F475677260440590%2F&show_text=false&width=560&t=0" style="border:none;overflow:hidden" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
 		</div>
+		<div class="col col-12 col-lg-4 col-sm-6">
+			<h5 class="text-light text-center h5">Cómo componer una canción</h5>
+			<iframe src="https://www.youtube.com/embed/-UG0enOsEAc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+		<div class="col col-12 col-lg-4 col-sm-6">
+			<h5 class="text-light text-center h5">Cómo componer una canción, 2da parte</h5>
+			<iframe src="https://www.youtube.com/embed/Hxc-wVXECrQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
 	</div>
 </div>
 @endsection
